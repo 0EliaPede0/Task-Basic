62469de815fbb24e9d21c5ac55bbd6057da5b3cc
0EliaPede0 <peddyelia@gmail.com>
2022-06-24 16:32:27 +0200

    Initial commit

---
+++ Bangladino/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bangladino.Models
+{
+    public partial class Task
+    {
+        public int Id { get; set; }
+        public string? Testo { get; set; }
+    }
+}
---
+++ Bangladino/Models/Utenti.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bangladino.Models
+{
+    public partial class Utenti
+    {
+        public int Id { get; set; }
+        public string NomeUtente { get; set; } = null!;
+    }
+}
